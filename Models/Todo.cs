a38a02316af7ee35f51523b7707cec2dbafeefa3
LiamBartolini <liambartolini@gmail.com>
2021-11-14 17:41:33 +0100

    added db & scaffold it, added refereces

---
+++ Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace todo_console_app.Models
+{
+    public partial class Todo
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Data { get; set; }
+        public long? Checked { get; set; }
+    }
+}
